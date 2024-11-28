6b30cc4baeda99ec1ca1cb8af57eeaa0cd1cde87
Maxim <113434750+maximfurzikovv@users.noreply.github.com>
2024-11-28 13:36:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const socket = require('socket.io');
 const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
 const Chess = require('chess.js').Chess;
 
 const app = express();
@@ -10,9 +11,45 @@ const io = socket(server);
 
 const PORT = process.env.PORT || 3000;
 
-// Словарь для хранения активных игр
-let games = {};
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/room/:roomCode', (req, res) => {
+//     const roomCode = req.params.roomCode;
+//
+//     // Получение информации о комнате из базы данных
+//     getRoomByCode(roomCode, (room) => {
+//         if (room) {
+//             console.log(`Комната найдена: ${room.room_code}`);
+//             res.sendFile(path.join(__dirname, 'public', 'index.html'));
+//         } else {
+//             console.log(`Комната не найдена: ${roomCode}`);
+//             res.status(404).send('Комната не найдена');
+//         }
+//     });
+// });
+app.get('/webapp', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Функция для получения информации о комнате из базы данных
+function getRoomByCode(roomCode, callback) {
+    const db = new sqlite3.Database('database/chess_game.db');
+
+    db.get('SELECT * FROM rooms WHERE room_code = ?', [roomCode], (err, row) => {
+        if (err) {
+            console.error('Ошибка при получении данных:', err);
+            callback(null);
+        } else {
+            callback(row);
+        }
+    });
+
+    db.close();
+}
+
+const games = {};
+
 io.on('connection', (socket) => {
     console.log('Новый игрок подключен:', socket.id);
 
@@ -36,7 +73,10 @@ io.on('connection', (socket) => {
             console.log('Этот игрок уже присоединился к игре.');
             return;
         }
-
+        // if (game.players.length === 2) {
+        //     socket.emit('error', 'Эта игра уже заполнена.');
+        //     return;
+        // }
         // Присваивание игроку цвета в зависимости от порядка подключения
         if (game.players.length === 0) {
             game.playerColors[socket.id] = 'w';
@@ -57,19 +97,24 @@ io.on('connection', (socket) => {
 
     socket.on('move', ({gameId, move}) => {
         const game = games[gameId];
+        console.log('Received move:', move);
         if (game) {
-            // Обновление состояния игры
-            const chessMove = game.game.move(move);
-            if (chessMove === null) {
-                console.log('Ошибка: Невалидный ход:', move);
-                return;
-            }
+            try {
+                const chessMove = game.game.move(move);
+                if (chessMove === null) {
+                    console.log('Ошибка: Невалидный ход:', move);
+                    return;
+                }
 
-            io.to(gameId).emit('updateBoard', game.game.fen()); // Отправка новое состояние игры
-            io.to(gameId).emit('updateHistory', game.game.history()); // Отправка истории ходов
-            console.log('Новое состояние доски отправлено:', game.game.fen());
+                io.to(gameId).emit('updateBoard', game.game.fen());
+                io.to(gameId).emit('updateHistory', game.game.history());
+                console.log('Новое состояние доски отправлено:', game.game.fen());
+            } catch (error) {
+                console.error('Ошибка при выполнении хода:', error);
+            }
         }
     });
+
 
     // Обработка обновления состояния доски
     socket.on('updateBoard', function (fen) {
@@ -91,7 +136,6 @@ io.on('connection', (socket) => {
     });
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
 
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
